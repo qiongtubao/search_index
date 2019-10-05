d9c0c878b16e11ed922f0ea1f01db6894318fa43
qiongtubao <810776886@qq.com>
2019-10-05 20:18:45 +0800

    ManagedDirectory实现Directory

--- src/directory/managed/mod.rs
+++ src/directory/managed/mod.rs
@@ -1,12 +1,12 @@
 use crate::directory::lib::Directory;
-use std::sync::{RwLock, Arc};
+use std::sync::{RwLock, Arc, RwLockWriteGuard};
 use std::collections::HashSet;
 use std::path::{PathBuf, Path};
 use crate::Result;
 use crate::directory::MANAGED_FILEPATH;
 use crate::directory::error::OpenReadError;
 use crate::error::DataCorruption;
-
+use std::io::Write;
 #[derive(Debug, Default)]
 struct MetaInformation {
     managed_paths: HashSet<PathBuf>,
@@ -17,6 +17,17 @@ pub struct ManagedDirectory {
     //为什么用box?
     directory: Box<dyn Directory>,
     meta_informations: Arc<RwLock<MetaInformation>>,
+}
+
+fn is_managed(path: &Path) -> bool {
+    path.to_str().map(|p_str| !p_str.starts_with(".")).unwrap_or(true)
+}
+
+fn save_managed_paths(directory: &mut dyn Directory, wlock: &RwLockWriteGuard<'_, MetaInformation>) -> std::io::Result<()> {
+    let mut w = serde_json::to_vec(&wlock.managed_paths)?;
+    writeln!(&mut w)?;
+    directory.atomic_write(&MANAGED_FILEPATH, &w[..])?;
+    Ok(())
 }
 
 impl ManagedDirectory {
@@ -48,6 +59,22 @@ impl ManagedDirectory {
             Err(OpenReadError::IOError(e)) => Err(From::from(e)),
         }
     }
+    fn register_file_as_managed(&mut self, filepath: &Path) -> std::io::Result<()> {
+        //暂时还不理解为什么用.来判断
+        if !is_managed(filepath) {
+            return Ok(())
+        }
+        let mut meta_wlock = self.meta_informations
+            .write().expect("Managed file lock poisoned");
+        //数据内添加filepath
+        let has_changed = meta_wlock.managed_paths.insert(filepath.to_owned());
+        if has_changed {
+            //写入文件
+            save_managed_paths(self.directory.as_mut(), &meta_wlock)?;
+        }
+        Ok(())
+    }
+
 }
 
 //Index clone
@@ -63,14 +90,15 @@ impl Clone for ManagedDirectory {
 
 impl Directory for ManagedDirectory {
     fn atomic_read(&self, path: &Path) -> std::result::Result<Vec<u8>, OpenReadError> {
-        unimplemented!()
+        self.directory.atomic_read(path)
     }
 
     fn atomic_write(&mut self, path: &Path, data: &[u8]) -> std::io::Result<()> {
-        unimplemented!()
+        self.register_file_as_managed(path)?;
+        self.directory.atomic_write(path, data)
     }
 
     fn exists(&self, path: &Path) -> bool {
-        unimplemented!()
+        self.directory.exists(path)
     }
 }
